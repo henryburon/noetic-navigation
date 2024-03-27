d1318779211ae3d02a3d72a91ec09ab375f85a6a
Henry Buron <henryburon34@gmail.com>
2024-03-26 20:02:34 -0500

    Orient is working well, as is the move to goal.  The next step is to integrate the two.  This will involve a few steps:

--- src/turtlebot_navigate.cpp
+++ src/turtlebot_navigate.cpp
@@ -7,20 +7,17 @@
 // Global variables
 ros::Publisher cmd_vel_pub;
 ros::Subscriber laser_sub;
-double goal_x, goal_y;
-double angle, x, y;
-double front_scan, right_scan, left_scan, back_scan;
-bool facing_goal, orientation_fixed, wall_ahead = false;
-bool goal_reached, clear_goal_path = false;
+double goal_x, goal_y, current_angle, current_x, current_y;
+double front_scan, right_scan, left_scan;
+double orient_tolerance = 10.0, goal_tolerance = 0.3;
+bool facing_goal = false;
 
-// Function prototypes
-bool orientToGoal(double tolerance_angle, tf::StampedTransform transform);
+
+// Function declarations
 void publishCmdVelocity(double linear_x, double angular_z);
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg);
-double measureDistance(double x1, double y1, double x2, double y2);
-void checkGoalReached();
+bool orientToGoal(double goal_x, double goal_y, double current_x, double current_y, double current_angle, double orient_tolerance);
 
-/// \brief Enum to represent the state of the robot
 enum State {
    IDLE,
    DRIVING_TO_GOAL,
@@ -28,15 +25,12 @@ enum State {
    GOAL_REACHED
 };
 
-// State variables
 State current_state = IDLE;
 
-// Main function
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
    ros::init(argc, argv, "turtlebot_navigate");
    ros::NodeHandle n;
-   ros::Rate loop_rate(10);
+   ros:: Rate loop_rate(10);
 
    // Publishers
    cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
@@ -48,19 +42,16 @@ int main(int argc, char **argv)
    n.getParam("/goal_x", goal_x);
    n.getParam("/goal_y", goal_y);
 
-   // Set the initial state
+   // Log initialization values
+   ROS_INFO("Goal: (%f, %f)", goal_x, goal_y);
+   ROS_INFO("Initial position: (%f, %f)", current_x, current_y);
+
    current_state = DRIVING_TO_GOAL;
 
-   // Log the goal and initial position
-   ROS_INFO("Goal: (%f, %f)", goal_x, goal_y);
-   ROS_INFO("Initial position: (%f, %f)", x, y);
-   ROS_INFO("Navigation started.");
+   while (ros::ok()) {
 
-
-   // Main timer loop
-   while (ros::ok()) 
-   {
       // Get the current position and orientation of the robot each loop
+      // ###################################################
       tf::TransformListener listener;
       tf::StampedTransform transform;
       try {
@@ -72,190 +63,143 @@ int main(int argc, char **argv)
          continue;
       }
       tf::Quaternion q = transform.getRotation();
-      angle = tf::getYaw(q);
-      x = transform.getOrigin().x();
-      y = transform.getOrigin().y();
-      
-      // State Machine
+      current_angle = tf::getYaw(q);
+      current_x = transform.getOrigin().x();
+      current_y = transform.getOrigin().y();
+      // ###################################################
+
+      // State machine
       switch (current_state) {
-
-         // Idle state, do nothing
          case IDLE:
-            break;
-
-         // Drive to goal
-         case DRIVING_TO_GOAL:
-            checkGoalReached();
-            facing_goal = orientToGoal(0.35, transform);
-            if (facing_goal) {
-               if (measureDistance(x, y, goal_x, goal_y) > 1.0) {
-                  publishCmdVelocity(0.20, 0.0);
-               } else {
-                  publishCmdVelocity(0.05, 0.0);
-               }
-            }
-            break;
-
-         // Maintain left scan distance from the wall and navigate around obstacles
-         case FOLLOWING_PERIMETER:
-
-            // If wall in front, and orientation not fixed, fit it
-            if (front_scan < 0.5 && orientation_fixed == false) {
-               publishCmdVelocity(0.0, -0.20);
-               break;
-            }
-            else {
-               orientation_fixed = true;
-            }
-
-            // If wall is detected on the left...
-            if (left_scan < 0.8) {
-               ROS_INFO("Following the left wall...");
-
-               // Maintain left scan distance from the wall as robot moves along the wall
-               if (orientation_fixed && front_scan > 0.5 && wall_ahead == false) {
-                  ROS_INFO("Maintaining distance...");
-                  if (left_scan < 0.35) {
-                     publishCmdVelocity(0.13, -0.09);
-                  }
-                  else if (left_scan >= 0.35) {
-                     publishCmdVelocity(0.13, 0.09);
-                  }
-                  else {
-                     publishCmdVelocity(0.13, 0.0);
-                  }
-               }
-
-               // If obstacle detected ahead while moving along the wall, stop
-               if (orientation_fixed && front_scan < 0.5 && wall_ahead == false) {
-                  publishCmdVelocity(0.0, 0.0);
-                  wall_ahead = true;
-               }
-
-               // If obstacle detected ahead while moving along the wall, stop and turn
-               if (wall_ahead && front_scan < 2.0) {
-                  publishCmdVelocity(0.0, -0.20);
-               }
-               // If reoriented, mark wall as not ahead anymore
-               else if (wall_ahead && front_scan > 2.0) {
-                  wall_ahead = false;
-                  orientation_fixed = false;
-                  publishCmdVelocity(0.0, 0.0);
-               }
-            }
-            else {
-               // If lost the wall, turn left until left scan detects the wall
-               ROS_INFO("Lost the wall, turning left...");
-               orientation_fixed = false;
-               wall_ahead = false;
-               publishCmdVelocity(0.10, 0.75);
-            }
-            break;
-
-         // Goal reached
-         case GOAL_REACHED:
+            // Do nothing
             publishCmdVelocity(0.0, 0.0);
-            if (!goal_reached) {
-            ROS_INFO("Goal reached.");
-            goal_reached = true;
-            }
             break;
-               }
 
-   ros::spinOnce();
-   loop_rate.sleep();
+         case DRIVING_TO_GOAL:
+
+            // Check if the goal is reached
+               // If yes, change state to GOAL_REACHED
+               // If no, continue
+            // Check if facing the goal
+               // If yes, drive forward
+               // If no, rotate to face the goal
+            
+
+
+            
+            facing_goal = orientToGoal(goal_x, goal_y, current_x, current_y, current_angle, 8.0);
+            
+            if (facing_goal) {
+               // Drive forward
+               publishCmdVelocity(0.20, 0.0);
+            }
+
+            
+
+
+            break;
+
+         case FOLLOWING_PERIMETER:
+            break;
+
+         case GOAL_REACHED:
+            // Stop the robot
+            publishCmdVelocity(0.0, 0.0);
+            ROS_INFO("Goal reached within a tolerance of %f meters", goal_tolerance);
+
+            break;
+      }
+
+
+
+
+      ros::spinOnce();
+      loop_rate.sleep();
    }
    return 0;
 }
 
-/// \brief Callback function for the laser scan data
-/// \param scan_msg The message containing the laser scan data
-/// \return void
-/// \details This function is called whenever a new laser scan message is received. It checks if the path to the goal is clear
-///          and sets the current state to DRIVING_TO_GOAL if the path is clear.
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg) {
 
-   // Find the scan to look at for checking if the path is clear
-   double abs_angle_to_goal = atan2(goal_y - y, goal_x - x) * 180 / M_PI;
-   double robot_angle = angle * (180/M_PI);
-   double angle_diff_1 = abs_angle_to_goal - robot_angle;
-   int rounded_angle_diff_1 = static_cast<int>(std::round(angle_diff_1));
-   // Sometimes the rounded angle difference is negative, so add 360 to it
-   if (rounded_angle_diff_1 < 0) {
-      rounded_angle_diff_1 += 360;
+   // Get the distance to the wall on each side of the robot
+   front_scan = scan_msg->ranges[0];
+   right_scan = scan_msg->ranges[270];
+   left_scan = scan_msg->ranges[90];
+
+   // Check if the path to the goal from the current position is clear (checks 1 meter in front of the robot)
+   double angle_to_goal = atan2(goal_y - current_y, goal_x - current_x) * 180 / M_PI;
+   double robot_angle = current_angle * 180 / M_PI;
+   double angle_diff = angle_to_goal - robot_angle;
+   int rounded_angle_diff = static_cast<int>(std::round(angle_diff));
+   if (rounded_angle_diff < 0) {
+      rounded_angle_diff += 360;
    }
 
-   // Check if the path to the goal is clear
-   bool is_path_clear = true;
-   for (int i = rounded_angle_diff_1 - 6; i <= rounded_angle_diff_1 + 6; i++) {
+   bool clear_path = true;
+   for (int i = rounded_angle_diff - 6; i <= rounded_angle_diff + 6; i++) {
       int index = (i + 360) % 360; // Ensure the index is within [0, 359]
-      if (scan_msg->ranges[index] <= 3.0) {
-         is_path_clear = false;
+      if (scan_msg->ranges[index] <= 1.0) { // Checks 1 meter in front of the robot
+         clear_path = false;
          break;
       }
    }
 
-   // If the path to the goal is clear, set the current state to DRIVING_TO_GOAL
-   if (is_path_clear) {
-      if (current_state != GOAL_REACHED) {
-         ROS_INFO("Path to goal appears clear.");
-         current_state = DRIVING_TO_GOAL;
-      }
+   // If 1 meter in the direction of the goal is clear, DRIVE TO GOAL
+   if (clear_path) {
+      current_state = DRIVING_TO_GOAL;
    }
 
-   // Get the distance to the wall in front of the robot
-   front_scan = scan_msg->ranges[0];
-   right_scan = scan_msg->ranges[270];
-   left_scan = scan_msg->ranges[90];
-   back_scan = scan_msg->ranges[180];
+
+
+
+   }
+
+   // Check if the path to the goal is clear
+      // If yes, change state to DRIVING_TO_GOAL
+
+
+
+   // Check if the path to the goal is clear
+      // If yes, change state to DRIVING_TO_GOAL
+      // If no, change state to FOLLOWING_PERIMETER
+
    
-   // If the robot is not following the perimeter and a wall is detected in front of the robot, set the current state to FOLLOWING_PERIMETER
-   if (front_scan < 0.5 && current_state != FOLLOWING_PERIMETER) {
-      ROS_INFO("Perimeter detected in front");
-      publishCmdVelocity(0.0, 0.0);
-      orientation_fixed = false;
-      current_state = FOLLOWING_PERIMETER;
+
+bool orientToGoal(double goal_x, double goal_y, double current_x, double current_y, double current_angle, double orient_tolerance) {
+   
+   // Calculate the angle the robot needs to rotate to face the goal
+   double angle_to_goal = atan2(goal_y - current_y, goal_x - current_x) * 180 / M_PI;
+   double robot_angle = current_angle * 180 / M_PI;
+   double angle_diff = angle_to_goal - robot_angle;
+
+   // Normalize the angle difference to be within [-180, 180]
+   if (angle_diff > 180) {
+      angle_diff -= 360;
+   } else if (angle_diff < -180) {
+      angle_diff += 360;
    }
-}
 
-/// \brief Function to orient the robot towards the goal
-/// \param tolerance_angle The tolerance angle for the orientation
-/// \param transform The transform containing the current position and orientation of the robot
-/// \return bool True if the robot is oriented towards the goal, false otherwise
-/// \details This function orients the robot towards the goal. It calculates the angle to the goal and the angle difference
-///          between the robot's orientation and the angle to the goal. If the angle difference is greater than the tolerance
-///          angle, the robot rotates towards the goal. The rotation speed is proportional to the angle difference. If the
-///          distance to the goal is less than 1.0, the rotation speed is reduced. The function returns true if the robot is
-///          oriented towards the goal, and false otherwise.
-bool orientToGoal(double tolerance_angle, tf::StampedTransform transform) {
+   // log angle difference
+   ROS_INFO("Angle difference: %f", angle_diff);
 
-   double angle_to_goal = atan2(goal_y - transform.getOrigin().y(), goal_x - transform.getOrigin().x());
-   double angle_diff = angle_to_goal - transform.getRotation().getAngle();
-   angle_diff = atan2(sin(angle_diff), cos(angle_diff));
-
-   // Rotation speed is proportional to angle_diff
-   if (fabs(angle_diff) > tolerance_angle) {
-      double rotation_speed = 0.25 * fabs(angle_diff);
-      if (measureDistance(x, y, goal_x, goal_y) < 1.0) {
-         rotation_speed = rotation_speed * 0.25;
-      }
-      if (angle_diff > 0) {
-         publishCmdVelocity(0.0, rotation_speed);
-      } else {
-         publishCmdVelocity(0.0, -rotation_speed);
-      }
+   // Publish the appropriate cmd_vel based on the angle difference
+   if (angle_diff > orient_tolerance) {
+      // Rotate CCW
+      publishCmdVelocity(0.0, 0.20);
+      return false;
+   }
+   else if (angle_diff < -orient_tolerance) {
+      // Rotate CW
+      publishCmdVelocity(0.0, -0.20);
       return false;
    }
    else {
+      // Stop rotating
+      // publishCmdVelocity(0.0, 0.0);
       return true;
    }
 }
 
-/// \brief Function to publish the command velocity message
-/// \param linear_x The linear velocity in the x-direction
-/// \param angular_z The angular velocity in the z-direction
-/// \details This function publishes the command velocity message on the /cmd_vel topic. The linear and angular velocities
-///          are set in the message and published.
 void publishCmdVelocity(double linear_x, double angular_z) {
       geometry_msgs::Twist msg;
 
@@ -271,26 +215,4 @@ void publishCmdVelocity(double linear_x, double angular_z) {
 
       // Publish the message on /cmd_vel topic
       cmd_vel_pub.publish(msg);
-}
-
-/// \brief Function to measure the distance between two points
-/// \param x1 The x-coordinate of the first point
-/// \param y1 The y-coordinate of the first point
-/// \param x2 The x-coordinate of the second point
-/// \param y2 The y-coordinate of the second point
-/// \return double The distance between the two points
-double measureDistance(double x1, double y1, double x2, double y2) {
-    double dx = x2 - x1;
-    double dy = y2 - y1;
-    return std::sqrt(dx * dx + dy * dy);
-}
-
-/// \brief Function to check if the goal has been reached
-/// \details This function checks if the goal has been reached. It calculates the distance to the goal and if the distance
-///          is less than 0.3, the current state is set to GOAL_REACHED.
-void checkGoalReached() {
-   double distance_to_goal = measureDistance(x, y, goal_x, goal_y);
-   if (distance_to_goal < 0.2) {
-      current_state = GOAL_REACHED;
-   }
 }
