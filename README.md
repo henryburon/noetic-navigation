baced5f454898cf50817ebefaede1a04eb28e1aa
Henry Buron <henryburon34@gmail.com>
2024-03-27 00:00:12 -0500

    Successful run to new goal position. Also adding video

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Launch simulation: ```roslaunch diff_drive_package launch_diff_drive_robot.launc
 
 Launch navigation node: ```rosrun diff_drive_package turtlebot_navigate```
 
-[goal_reached.webm](https://github.com/henryburon/noetic-navigation/assets/141075086/9ae4324a-96ad-484b-ba17-f4ce05c5a26d)
+[turtlebot_navigate.webm](https://github.com/henryburon/noetic-navigation/assets/141075086/458e1778-987b-4350-82e1-822f6de75532)
--- src/turtlebot_navigate.cpp
+++ src/turtlebot_navigate.cpp
@@ -4,17 +4,18 @@
 #include <tf/transform_listener.h>
 #include <cmath>
 
+
 // Global variables
 ros::Publisher cmd_vel_pub;
 ros::Subscriber laser_sub;
 double goal_x, goal_y, current_angle, current_x, current_y;
 double front_obstacle, right_obstacle, left_obstacle;
 double front_scan, right_scan, left_scan;
-double orient_tolerance = 10.0, goal_tolerance = 0.3;
+double orient_tolerance = 10.0, goal_tolerance = 0.1;
 bool facing_goal, goal_reached = false;
 bool clear_path_to_goal = false;
 bool initial_orientation_fixed, orientation_fixed = false;
-
+bool state_switch = true;
 
 // Function declarations
 void publishCmdVelocity(double linear_x, double angular_z);
@@ -23,6 +24,8 @@ bool orientToGoal(double goal_x, double goal_y, double current_x, double current
 bool checkGoalCondition(double goal_x, double goal_y, double current_x, double current_y, double goal_tolerance);
 void followPerimeter();
 
+
+/// \brief Enum to represent the state of the robot
 enum State {
    IDLE,
    DRIVING_TO_GOAL,
@@ -30,8 +33,12 @@ enum State {
    GOAL_REACHED
 };
 
+// Initialize state variables
 State current_state = IDLE;
+State old_state = IDLE;
 
+
+/// \brief Main function
 int main(int argc, char **argv) {
    ros::init(argc, argv, "turtlebot_navigate");
    ros::NodeHandle n;
@@ -52,11 +59,11 @@ int main(int argc, char **argv) {
    ROS_INFO("Initial position: (%f, %f)", current_x, current_y);
 
    current_state = DRIVING_TO_GOAL;
+   old_state = DRIVING_TO_GOAL;
 
    while (ros::ok()) {
 
       // Get the current position and orientation of the robot each loop
-      // ###################################################
       tf::TransformListener listener;
       tf::StampedTransform transform;
       try {
@@ -71,7 +78,12 @@ int main(int argc, char **argv) {
       current_angle = tf::getYaw(q);
       current_x = transform.getOrigin().x();
       current_y = transform.getOrigin().y();
-      // ###################################################
+
+      // Check if the goal has been reached every loop
+      checkGoalCondition(goal_x, goal_y, current_x, current_y, goal_tolerance);
+      if (old_state != current_state) {
+         state_switch = true;
+      }
 
       // State machine
       switch (current_state) {
@@ -81,8 +93,11 @@ int main(int argc, char **argv) {
             break;
 
          case DRIVING_TO_GOAL:
-         ROS_INFO("Driving to goal");
-
+         if (state_switch) {
+            ROS_INFO("Driving to goal");
+            state_switch = false;
+         }
+            // Orient to goal first
             facing_goal = orientToGoal(goal_x, goal_y, current_x, current_y, current_angle, 8.0);
             if (facing_goal) {
                // Drive forward
@@ -91,7 +106,10 @@ int main(int argc, char **argv) {
             break;
 
          case FOLLOWING_PERIMETER:
-         ROS_INFO("Following perimeter");
+         if (state_switch) {
+            ROS_INFO("Following perimeter");
+            state_switch = false;
+         }
 
             if (clear_path_to_goal) {
                // Orient to goal first, to avoid re-activating the FOLLOWING_PERIMETER state
@@ -106,15 +124,15 @@ int main(int argc, char **argv) {
             break;
 
          case GOAL_REACHED:
-            // Stop the robot
-            publishCmdVelocity(0.0, 0.0);
-            ROS_INFO("Goal reached within a tolerance of %f meters", goal_tolerance);
-
+            if (state_switch) {
+               // Stop the robot
+               publishCmdVelocity(0.0, 0.0);
+               ROS_INFO("Goal reached within a tolerance of %f meters", goal_tolerance);
+               state_switch = false;
+            }
             break;
       }
-
-
-
+      old_state = current_state;
 
       ros::spinOnce();
       loop_rate.sleep();
@@ -122,6 +140,13 @@ int main(int argc, char **argv) {
    return 0;
 }
 
+/// \brief Callback function for the laser scan data
+/// \param scan_msg The message containing the laser scan data
+/// \return void
+/// \details This function is called every time a new laser scan message is received
+///         from the /scan topic. The function checks if there is an obstacle in front
+///        of the robot, on the left side, and on the right side. It also checks if the
+///        path to the goal from the current position is clear.
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg) {
    if (goal_reached) {
       return;
@@ -193,12 +218,14 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg) {
    }
 }
 
+/// \brief Function to make the robot follow the perimeter of an obstacle
+/// \return void
+/// \details This function is called when the robot is following the perimeter of an obstacle.
+///         The robot will first orient itself parallel to the obstacle, and then follow the
+///         perimeter by maintaining a distance of 0.5 meters from the obstacle on the right side.
 void followPerimeter() {
-   // orient parallel to the perimeter/wall/obstacle
 
-   ROS_INFO("Front scan: %f", front_scan);
-   ROS_INFO("Right scan: %f", right_scan);
-   
+   // Orient the robot parallel to the perimeter/wall/obstacle
    if (orientation_fixed == false) {
       if (front_scan < 1.0) {
          publishCmdVelocity(0.0, 0.20);
@@ -208,36 +235,36 @@ void followPerimeter() {
          publishCmdVelocity(0.0, 0.0);
       }
    }
-   else if (orientation_fixed) {
-      
 
-      ROS_INFO("Orientation fixed!");
-      
+   // Follow the perimeter of the obstacle
+   else if (orientation_fixed) {      
 
       // Maintain going straight, following object on the right
       if (right_scan > 0.5 && right_scan < 1.5) {
-         publishCmdVelocity(0.15, -0.10);
+         publishCmdVelocity(0.22, -0.10);
       }
       else if (right_scan <= 0.5) {
-         publishCmdVelocity(0.15, 0.10);
+         publishCmdVelocity(0.22, 0.10);
       }
 
       // Curve right, if no object on the right
       else if (right_scan >= 1.5) {
-         publishCmdVelocity(0.15, -0.25);
+         publishCmdVelocity(0.20, -0.25);
       }
    }   
 }
 
-   
-
-
-
-
-
-
-
-
+/// \brief Function to orient the robot to face the goal
+/// \param goal_x The x-coordinate of the goal
+/// \param goal_y The y-coordinate of the goal
+/// \param current_x The x-coordinate of the robot's current position
+/// \param current_y The y-coordinate of the robot's current position
+/// \param current_angle The orientation of the robot
+/// \param orient_tolerance The tolerance for the orientation
+/// \return bool Returns true if the robot is facing the goal, false otherwise
+/// \details This function calculates the angle the robot needs to rotate to face the goal.
+///         The robot will rotate either clockwise or counter-clockwise to face the goal.
+///         The function will return true if the robot is facing the goal within the tolerance.
 bool orientToGoal(double goal_x, double goal_y, double current_x, double current_y, double current_angle, double orient_tolerance) {
    
    // Calculate the angle the robot needs to rotate to face the goal
@@ -270,6 +297,11 @@ bool orientToGoal(double goal_x, double goal_y, double current_x, double current
    }
 }
 
+/// \brief Function to publish the cmd_vel message
+/// \param linear_x The linear velocity in the x-direction
+/// \param angular_z The angular velocity in the z-direction
+/// \return void
+/// \details This function publishes the cmd_vel message with the given linear and angular velocities.
 void publishCmdVelocity(double linear_x, double angular_z) {
       geometry_msgs::Twist msg;
 
@@ -287,6 +319,15 @@ void publishCmdVelocity(double linear_x, double angular_z) {
       cmd_vel_pub.publish(msg);
 }
 
+/// \brief Function to check if the goal has been reached
+/// \param goal_x The x-coordinate of the goal
+/// \param goal_y The y-coordinate of the goal
+/// \param current_x The x-coordinate of the robot's current position
+/// \param current_y The y-coordinate of the robot's current position
+/// \param goal_tolerance The tolerance for the goal
+/// \return bool Returns true if the goal has been reached, false otherwise
+/// \details This function calculates the distance between the robot's current position and the goal.
+///         If the distance is less than or equal to the goal tolerance, the goal has been reached.
 bool checkGoalCondition(double goal_x, double goal_y, double current_x, double current_y, double goal_tolerance) {
    double distance_to_goal = sqrt(pow(goal_x - current_x, 2) + pow(goal_y - current_y, 2));
    if (distance_to_goal <= goal_tolerance) {
